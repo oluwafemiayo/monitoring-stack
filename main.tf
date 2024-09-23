d1065295573cc6ba1f820507b7cb1253048ee9b5
oluwafemiayo <olufemi123ayo@gmail.com>
2024-09-23 04:33:54 +0200

    initial commit

--- main.tf
+++ main.tf
@@ -1,8 +1,9 @@
-# Include Prometheus and Grafana modules
+# Prometheus Modules
 module "prometheus" {
   source = "./module/prometheus-monitoring"
 }
 
+#Grafana Modules
 module "grafana" {
   source                 = "./module/grafana-monitoring"
   grafana_admin_user     = var.grafana_admin_user
